159ace7cbfaad0a0357b24710a6259804a9a36f8
Waldemar Porscha <wp@porscha.eu>
2024-07-19 08:10:00 +0200

    Updated library version

--- include/versions.h
+++ include/versions.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define MAIN_Version "b248288"
-#define KNX_Version "e69f636"
+#define MAIN_Version "0f27451"
+#define KNX_Version "2.1.0+e69f636"
 #define MODULE_LogicModule_Version "3.3.0+0834fc0"
 #define MODULE_LogicModule_ETS 51
-#define MODULE_Common_Version "1.2.0+73e09e3"
+#define MODULE_Common_Version "1.2.0+abed303"
 #define MODULE_Common_ETS 18
