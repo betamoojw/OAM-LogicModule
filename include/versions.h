a5e2ea145a7fa54a57ee8275213abc1bb91fb05c
Waldemar Porscha <wp@porscha.eu>
2024-07-19 10:38:47 +0200

    Updated lib with link to ConfigTransfer

--- include/versions.h
+++ include/versions.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define MAIN_Version "0f27451"
+#define MAIN_Version "159ace7"
 #define KNX_Version "2.1.0+e69f636"
 #define MODULE_LogicModule_Version "3.3.0+0834fc0"
 #define MODULE_LogicModule_ETS 51
-#define MODULE_Common_Version "1.2.0+abed303"
+#define MODULE_Common_Version "1.2.0+ddd1363"
 #define MODULE_Common_ETS 18
