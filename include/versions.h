fbb0ecc1b085d4a25a92b7dab399bfce6549ff01
Waldemar Porscha <wp@porscha.eu>
2024-07-25 17:36:41 +0200

    Release 3.1.1

--- include/versions.h
+++ include/versions.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define MAIN_Version "159ace7"
+#define MAIN_Version "a5e2ea1"
 #define KNX_Version "2.1.0+e69f636"
-#define MODULE_LogicModule_Version "3.3.0+0834fc0"
+#define MODULE_LogicModule_Version "3.3.1+b2c81b7"
 #define MODULE_LogicModule_ETS 51
-#define MODULE_Common_Version "1.2.0+ddd1363"
+#define MODULE_Common_Version "1.2.0+dd5b9fb"
 #define MODULE_Common_ETS 18
