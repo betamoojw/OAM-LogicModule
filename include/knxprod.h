18ae307cf25d3ed3b8a36b35ac2b9cc9ce47048e
Waldemar Porscha <wp@porscha.eu>
2024-12-12 08:45:57 +0100

    Release 3.5.2: - Fix Logic repeat filter ETS problem - Unicast auto ack - ETS 6.3.0 compatible installation scripts

--- include/knxprod.h
+++ include/knxprod.h
@@ -10,13 +10,13 @@
                                              
 #define MAIN_OpenKnxId 0xA0
 #define MAIN_ApplicationNumber 48
-#define MAIN_ApplicationVersion 52
+#define MAIN_ApplicationVersion 53
 #define MAIN_ParameterSize 11475
 #define MAIN_MaxKoNumber 316
 #define MAIN_OrderNumber "OpenKnxLogic"
 #define BASE_ModuleVersion 18
 #define UCT_ModuleVersion 2
-#define LOG_ModuleVersion 52
+#define LOG_ModuleVersion 53
 // Parameter with single occurrence
 
 
@@ -1365,9 +1365,9 @@
 #define LOG_fOOutputFilter                      53      // 2 Bits, Bit 4-3
 #define     LOG_fOOutputFilterMask 0x18
 #define     LOG_fOOutputFilterShift 3
-#define LOG_fOSendOnChange                      53      // 1 Bit, Bit 3
-#define     LOG_fOSendOnChangeMask 0x08
-#define     LOG_fOSendOnChangeShift 3
+#define LOG_fOSendOnChange                      53      // 1 Bit, Bit 2
+#define     LOG_fOSendOnChangeMask 0x04
+#define     LOG_fOSendOnChangeShift 2
 #define LOG_fODpt                               54      // 8 Bits, Bit 7-0
 #define LOG_fOOn                                55      // 8 Bits, Bit 7-0
 #define LOG_fOOnBuzzer                          55      // 8 Bits, Bit 7-0
--- include/versions.h
+++ include/versions.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define MAIN_Version "a79bc1a"
-#define KNX_Version "2.1.1+f606f6d"
-#define MODULE_LogicModule_Version "3.4.0+2369ceb"
-#define MODULE_LogicModule_ETS 52
-#define MODULE_Common_Version "1.2.0+6f80043"
+#define MAIN_Version "78a54c8"
+#define KNX_Version "2.1.2+764900e"
+#define MODULE_LogicModule_Version "3.5.2+7bc407d"
+#define MODULE_LogicModule_ETS 53
+#define MODULE_Common_Version "1.2.0+5d0a279"
 #define MODULE_Common_ETS 18
